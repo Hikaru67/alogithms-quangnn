74e021b1b862ab0cd40ad271e6e6b28078d1ae52
Hikaru67 <shinigamii.hikaru@gmail.com>
2021-02-17 16:54:00 +0700

    6th

---
+++ Bai12sortByName.php
@@ -0,0 +1,28 @@
+<?php
+/*"Hãy viết function sortByName(listProduct)
+trả về danh sách product sắp xếp độ dài của tên từ cao đến thấp.
+Làm theo thuật toán insertion."*/
+
+include 'common/autoload.php';
+$listProduct = PRODUCTS;
+
+function sortByName(&$listProduct){
+    $amountProducts = count($listProduct);
+
+    for($i=0; $i<$amountProducts; $i++){
+        $valueToInsert = $listProduct[$i];
+        $holePosition = $i;
+        while($holePosition > 0 && strcmp($listProduct[$holePosition-1]['name'], $valueToInsert['name'])>0){
+            echo strcmp($listProduct[$holePosition-1]['name'], $valueToInsert['name']." ");
+            $listProduct[$holePosition] = $listProduct[$holePosition-1];
+            $holePosition--;
+        }
+
+        $listProduct[$holePosition] = $valueToInsert;
+
+    }
+
+    return $listProduct;
+}
+
+print_r(sortByName($listProduct));
